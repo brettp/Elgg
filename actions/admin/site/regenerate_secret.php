0414a03eaa2a6833f7508403f00d32788f0a7a92
Brett Profitt <brett.profitt@gmail.com>
2014-02-12 17:26:03 -0500

    chore(admin): Moved site secret regeneration to advanced settings.
    Fixes #6443.

--- actions/admin/site/regenerate_secret.php
+++ actions/admin/site/regenerate_secret.php
@@ -8,4 +8,12 @@ elgg_reset_system_cache();
 
 system_message(elgg_echo('admin:site:secret_regenerated'));
 
+if (elgg_is_xhr()) {
+	$ts = time();
+	echo json_encode(array(
+		'__elgg_ts' => $ts,
+		'__elgg_token' => generate_action_token($ts)
+	));
+}
+
 forward(REFERER);
